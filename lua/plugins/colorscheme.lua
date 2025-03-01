13f33343c130266ced66945342f29cfe6f59f75a
Dakota <33270107+dakoders@users.noreply.github.com>
2025-02-28 16:57:04 -0800

    (feat): add oil and snacks to nvim (#3)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,4 @@
 return {
-  -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   {
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -14,10 +9,6 @@ return {
           comments = { italic = true }, -- Disable italics in comments
         },
       }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-moon'
     end,
   },
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    keys = {
+      { '-', '<CMD>Oil<CR>', desc = 'Open parent directory', mode = 'n' },
+      {
+        '<leader>-',
+        function()
+          require('oil').toggle_float()
+        end,
+        desc = 'Open parent directory',
+        mode = 'n',
+      },
+    },
+  },
+}
